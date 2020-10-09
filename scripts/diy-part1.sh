b4283f942400b43e5a0ac6e096b0846ef96beac7
CheungTOYGSWIFG <40869522+cheungnut@users.noreply.github.com>
2020-10-09 17:00:40 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -15,7 +15,13 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git cheungnut https://github.com/cheungnut/CHEUNG-OpenWRT-app-packages' feeds.conf.default
+sed -i '$a src-git appoaf https://github.com/cheungnut/CHEUNG-OpenWRT-app-OpenAppFilter' feeds.conf.default
+sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-package' feeds.conf.default
+sed -i '$a src-git ctheme https://github.com/cheungnut/CHEUNG-OpenWRT-Theme' feeds.conf.default
+# sed -i '$a src-git jerrykuku https://github.com/jerrykuku/luci-theme-argon' feeds.conf.default
 
 # Add luci-theme-argon
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
-rm -rf ../lean/luci-theme-argon
+# git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+# rm -rf ../lean/luci-theme-argon
